bdbbf24b78a946d8810413439c7c56d6f7cda635
weiyafei <2553241022@qq.com>
2019-06-10 21:20:55 +0800

    修改vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import './plugins/ant-design-vue.js'
+import store from './store'
 
 Vue.config.productionTip = false;
 
