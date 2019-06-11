e7dd0fcf8ae07870b7304c8d32026a67bd66f74c
weiyafei <2553241022@qq.com>
2019-06-11 17:28:06 +0800

    调试modular-router备份

---
+++ src/views/map/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="map">
+
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+
+export default {
+    name: 'Map',
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+};
+</script>
