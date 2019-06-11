226ddeffb9dd8c900e47ca2e618701c4267cdf52
weiyafei <2553241022@qq.com>
2019-06-11 20:04:39 +0800

    修改vue.config.js备份

---
+++ src/layouts/laytop/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="laytop">
+
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+
+export default {
+    name: 'Laytop',
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+};
+</script>
