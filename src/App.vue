f51b74a12eac05f524ffeaaa2c751e3f18779a2d
weiyafei <2553241022@qq.com>
2019-06-12 11:34:02 +0800

    重新配置vue-ant保存

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
 };
 </script>
 <style lang="scss">
-@import '~@/scss/base.scss';
+// @import '~@/scss/base.scss';
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
