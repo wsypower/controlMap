0660bda5892530fda40eb1bcca3a581f35c47e7d
weiyafei <2553241022@qq.com>
2019-06-14 11:33:41 +0800

    修改了vue.config.js的注释

---
+++ src/utils/cookies.js
@@ -0,0 +1,53 @@
+/*
+ * @Author: wei.yafei
+ * @Date: 2019-06-04 21:25:15
+ * @Last Modified by: wei.yafei 
+ * @Last Modified time: 2019-06-14 11:19:36
+ */
+import Cookies from 'js-cookie'
+const cookies = {}
+
+/**
+ * @description 存储 cookie 值
+ * @author weiyafei
+ * @date 2019-06-12-11:12:27
+ * @param {String} name cookie name
+ * @param {String} value cookie value
+ * @param {Object} setting cookie setting
+ */
+cookies.set = function(name = 'default', value = '', cookieSetting = {}) {
+  let currentCookieSetting = {
+    expires: 1
+  }
+  Object.assign(currentCookieSetting, cookieSetting)
+  Cookies.set(`cgadmin-${process.env.VUE_APP_VERSION}-${name}`, value, currentCookieSetting)
+}
+
+/**
+ * @description 拿到 cookie 值
+ * @author weiyafei
+ * @date 2019-06-12-11:12:27
+ * @param {String} name cookie name
+ */
+cookies.get = function(name = 'default') {
+  return Cookies.get(`cgadmin-${process.env.VUE_APP_VERSION}-${name}`)
+}
+
+/**
+ * @description 拿到 cookie 全部的值
+ */
+cookies.getAll = function() {
+  return Cookies.get()
+}
+
+/**
+ * @description 删除 cookie
+ * @author weiyafei
+ * @date 2019-06-13-12:12:15
+ * @param {String} name cookie name
+ */
+cookies.remove = function(name = 'default') {
+  return Cookies.remove(`cgadmin-${process.env.VUE_APP_VERSION}-${name}`)
+}
+
+export default cookies
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,37 @@
+/*
+ * @Author: wei.yafei
+ * @Date: 2019-05-23 23:32:53
+ * @Last Modified by:   wei.yafei
+ * @Last Modified time: 2019-06-14 11:32:53
+ */
 /* eslint-disable no-unused-vars */
-let path = require('path')
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
+
+//使用uglify-js进行js文件的压缩。
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+// 拼接路径
+const resolve = dir => require('path').join(__dirname, dir)
+
+// 基础路径 注意发布之前要先修改这里
+let publicPath = '/'
+
 module.exports = {
+  /*
+   ** 项目部署基础
+   ** 默认情况下，我们假设你的应用将被部署在域的根目录下,
+   ** 例如：https://www.my-app.com/
+   ** 默认：'/'
+   ** 如果您的应用程序部署在子路径中，则需要在这指定子路径
+   ** 例如：https://www.foobar.com/my-app/
+   ** 需要将它改为'/my-app/'
+   */
+  publicPath, // 根据你的实际情况更改这里
+  outputDir: 'dist', //当运行 vue-cli-service build 时生成的生产环境构建文件的目录.
+  assetsDir: 'assets', //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录.
+  lintOnSave: true, //是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码.
   css: {
     loaderOptions: {
+      // 设置 scss 公用变量文件
       sass: {
         data: '@import "@/scss/settings.scss";'
       }
@@ -24,18 +50,5 @@ module.exports = {
   },
   devServer: {
     open: true
-  },
-  // 项目部署基础
-  // 默认情况下，我们假设你的应用将被部署在域的根目录下,
-  // 例如：https://www.my-app.com/
-  // 默认：'/'
-  // 如果您的应用程序部署在子路径中，则需要在这指定子路径
-  // 例如：https://www.foobar.com/my-app/
-  // 需要将它改为'/my-app/'
-  publicPath: '/',
-  outputDir: 'dist',
-  assetsDir: 'assets'
-  /*  lintOnSave: true,
-  productionSourceMap: true,
-  runtimeCompiler: true */
+  }
 }
