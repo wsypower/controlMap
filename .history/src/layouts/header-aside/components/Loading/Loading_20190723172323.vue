20edab349d0316f78b2461afe30c1cce908d4bfb
weiyafei <2553241022@qq.com>
2019-07-23 17:26:08 +0800

    loading组件完成

---
+++ .history/src/layouts/header-aside/components/Loading/Loading_20190723172323.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="spinning">
+    <a-spin spinning size="large" :indicator="indicator">
+      <slot></slot>
+    </a-spin>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Logo from '../components/loading-logo/LoadingLogo.vue'
+export default {
+  name: 'Loading',
+  data() {
+    return {
+      indicator: h => {
+        return <Logo></Logo>
+      }
+    }
+  },
+  computed: {
+    ...mapState('cgadmin', {
+      //页面加载提示 ====> login接口resolve后消失
+      Spinning: state => state.page.Spinning
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+/* ant-Vue spin修改样式*/
+.spinning{
+  
+}
+/deep/.ant-spin-nested-loading {
+  width: 100%;
+  height: 100%;
+}
+/deep/.ant-spin-container {
+  width: 100%;
+  height: 100%;
+}
+/deep/.ant-spin-nested-loading > div > .ant-spin {
+  display: block;
+  position: absolute;
+  height: 100%;
+  max-height: 100%;
+  width: 100%;
+  z-index: 4;
+}
+/deep/.ant-spin-nested-loading > div > .ant-spin .ant-spin-dot {
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  margin: 0px;
+}
+</style>
---
+++ .history/src/layouts/header-aside/components/components/loading-logo/LoadingLogo_20190723154916.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="logo">
+    <!-- 头部彩色条 -->
+    <header>
+      <div aria-busy="true" aria-label="Loading, please wait." role="progressbar"></div>
+    </header>
+    <main role="main">
+      <div class="loader">
+        <div class="loader_overlay"></div>
+        <div class="loader_cogs">
+          <!-- LOGO样式 -->
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'logo'
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  @function colour($value) {
+    @return map-get($colours, $value);
+  }
+  // 发光条高度变量
+  $preloader-height: 12px;
+  $colours: (
+    green: #5fa783,
+    red: #cf7063,
+    blue: #638ddb,
+    yellow: #ebc471
+  );
+  // 发光条速度变量
+  $animation-duration: 4.5s;
+  // 发光条
+  header [role='progressbar'][aria-busy='true'] {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding-top: $preloader-height;
+    width: 100%;
+    background-color: colour(green);
+    animation: preloader-background linear $animation-duration infinite;
+
+    &::before,
+    &::after {
+      display: block;
+      position: absolute;
+      top: 0;
+      z-index: 2;
+      width: 0;
+      height: $preloader-height;
+      background: #afa;
+      animation: preloader-front linear $animation-duration infinite;
+      content: '';
+    }
+
+    &::before {
+      right: 50%;
+    }
+
+    &::after {
+      left: 50%;
+    }
+  }
+
+ 
+  header {
+    position: relative;
+    width: 100%;
+    height: 60px;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+    flex: 1 1 auto;
+    text-align: center;
+    // background-color: red;
+
+    :root & > * + * {
+      margin-top: 1em;
+    }
+  }
+  .loader {
+    height: 100%;
+    position: relative;
+    margin: auto;
+    width: 400px;
+    &_overlay {
+      width: 150px;
+      height: 150px;
+      background: transparent;
+      box-shadow: 0px 0px 0px 1000px rgba(255, 255, 255, 0.67), 0px 0px 19px 0px rgba(0, 0, 0, 0.16) inset;
+      border-radius: 100%;
+      z-index: -1;
+    }
+    &_cogs {
+      z-index: -2;
+      width: 100px;
+      height: 100px;
+      top: -120px !important;
+    }
+  }
+}
+
+</style>
