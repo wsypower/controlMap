248a2f7e5bfd2dac618ded0b3f1610639975cac2
midoriruka <sijty@hotmail.com>
2019-07-25 09:26:41 +0800

    修改日期工具

--- src/utils/util.tool.js
+++ src/utils/util.tool.js
@@ -5,5 +5,5 @@
  */
 export function stampConvertToTime(time){
   const date = new Date(parseInt(time) + 8 * 3600 * 1000); // 增加8小时
-  return date.toJSON().substr(0, 19).replace('T', ' ');
+  return date.toJSON().substr(0, 10).replace('T', ' ');
 }
