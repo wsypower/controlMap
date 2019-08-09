facb550ae790089a486b7e7e2a0fa10932f0378e
midoriruka <sijty@hotmail.com>
2019-08-09 17:49:16 +0800

    增加地图部分

---
+++ src/utils/util.map.manage.js
@@ -0,0 +1,153 @@
+/**
+ * @description: 地图管理类
+ * @author:sijianting
+ * @createDate:2019/7/11 9:48
+ */
+import VectorLayer from 'ol/layer/Vector'
+import VectorSource from 'ol/source/Vector'
+import GeoJSON from 'ol/format/GeoJSON'
+import Draw from 'ol/interaction/Draw.js';
+import { Modify } from 'ol/interaction.js';
+import Feature from 'ol/Feature';
+import MultiPolygon from 'ol/geom/MultiPolygon';
+import { fromCircle } from 'ol/geom/Polygon'
+
+export class MapManager {
+  constructor(map) {
+    this.map = map;
+    this.drawLayer = null;
+  }
+  /**
+   * @description: 通过geojson格式数据添加矢量图层
+   * @param {geojson} geojson
+   * @param {style} style
+   */
+  addVectorLayerByGeoJSON(geojson, style) {
+    const source = new VectorSource({})
+    const vectorLayer = new VectorLayer({
+      source,
+      style
+    })
+    const features = new GeoJSON().readFeatures(geojson)
+    source.addFeatures(features)
+    this.map.addLayer(vectorLayer)
+    return vectorLayer
+  }
+  /**
+   * @description: 通过features数组添加矢量图层
+   * @param {Array} features
+   * @param {style} style
+   * @param {number} zIndex
+   */
+  addVectorLayerByFeatures(features, style, zIndex) {
+    const source = new VectorSource({})
+    const vectorLayer = new VectorLayer({
+      source,
+      style,
+      zIndex
+    })
+    source.addFeatures(features)
+    this.map.addLayer(vectorLayer)
+    return vectorLayer
+  }
+  /**
+   * @description: 定位
+   * @param  {Array} coord 经纬度
+   */
+  locateTo(coord) {
+    this.map.getView().animate({
+      center: coord,
+      zoom: 15,
+      duration: 500
+    })
+  }
+  /**
+   * @description: 激活绘制功能
+   * @param {String}type 绘制类型
+   * @param {Boolean}isFreeHand 是否自由绘制
+   * @return {*}
+   */
+  activateDraw(type) {
+    debugger;
+    let pointSource = new VectorSource({ wrapX: false });
+    let draw;
+    // let lineSource= new VectorSource({ wrapX: false });
+    // let polygonSource = new VectorSource({ wrapX: false });
+    if (type !== 'None') {
+      draw = new Draw({
+        source: pointSource,
+        type: type
+      });
+      this.map.addInteraction(draw);
+    }
+    return draw;
+  }
+  /**
+   * @description: 激活点绘制功能
+   * @return {*}
+   */
+  activePointDraw(){
+
+  }
+  /**
+   *  @description:取消激活绘制功能
+   */
+  inactivateDraw(draw) {
+    this.map.removeInteraction(draw)
+  }
+
+  /**
+   * @description:激活修改地图
+   * @author:sijianting
+   * @createDate:2019/7/26 9:52
+   */
+  activeModifyFeature(modify, modifySource){
+    if(modify){
+      this.inactivateDraw(modify);
+    }
+    modify = new Modify({ source: modifySource});
+    this.map.addInteraction(modify);
+    return modify;
+  }
+  /**
+   * @description:移除单个图层
+   * @param layer 需要移除的图层
+   */
+  removeLayer(layer) {
+    this.map.removeLayer(layer)
+  }
+  getMap(){
+    return this.map;
+  }
+}
+
+/**
+ * @description:过滤区域内点位的方法
+ * @author:sijianting
+ * @createDate:2019/7/25 14:56
+ */
+export function filterMeetingPeople(feature, points) {
+  const geo = feature.getGeometry();
+  const peoples = points.filter(p => {
+    if(geo.intersectsCoordinate(p.position)){
+      return p;
+    }
+  });
+  return peoples;
+}
+
+/**
+ * @description:圆转面保存
+ * @author:sijianting
+ * @createDate:2019/7/26 15:22
+ */
+export function circleToPloygon(feature) {
+  const polygon = fromCircle(feature.getGeometry(), 64,90);
+  let mutiPolygon = new MultiPolygon({});
+  mutiPolygon.appendPolygon(polygon);
+  feature = new Feature({
+    geometry: mutiPolygon
+  })
+  return feature;
+}
+
--- src/views/records/components/BaoZhangMapDialog.vue
+++ src/views/records/components/BaoZhangMapDialog.vue
@@ -1,108 +1,140 @@
 <template>
-    <a-modal title="保障视图" v-model="mapDialogVisible" width="100%"
-             wrapClassName="bao-zhang-map-dialog-wap"
-             :bodyStyle="{top:0+'px',height: '100%'}"
-             class="bao-zhang-map-dialog"
-             @cancel="handleCancel">
-        <div class="yuan_dialog_body">
-            <div class="operate-panel">
-            <a-button type="primary" @click="drawArea">多边形</a-button>
-            <a-button type="primary" @click="drawLine">线</a-button>
-            <a-button type="primary" @click="drawDot">点</a-button>
-            <a-button type="primary" @click="clearDraw">清楚选中区域</a-button>
-            </div>
-        </div>
-        <template slot="footer">
-            <a-button type="primary" @click="saveMap">保存视图</a-button>
-            <a-button @click="resetMap">重置视图</a-button>
-        </template>
-    </a-modal>
+  <a-modal
+    title="保障视图"
+    v-model="mapDialogVisible"
+    width="100%"
+    wrapClassName="bao-zhang-map-dialog-wap"
+    :bodyStyle="{ top: 0 + 'px', height: '100%' }"
+    class="bao-zhang-map-dialog"
+    @cancel="handleCancel"
+  >
+    <div class="yuan_dialog_body">
+      <!-- 地图控件注入地址 -->
+      <LayoutMap ref="olMap"></LayoutMap>
+      <div class="operate-panel">
+        <a-button type="primary" @click="drawDot">点</a-button>
+        <a-button type="primary" @click="drawLine">线</a-button>
+        <a-button type="primary" @click="drawArea">面</a-button>
+        <a-button type="primary" @click="clearDraw">清除选中图形</a-button>
+      </div>
+    </div>
+    <template slot="footer">
+      <a-button type="primary" @click="saveMap">保存视图</a-button>
+      <a-button @click="resetMap">重置视图</a-button>
+    </template>
+  </a-modal>
 </template>
 <script type="text/ecmascript-6">
-    export default{
-      name: 'baoZhangMapDialog',
-        props:{
-            visible: {
-                type: Boolean,
-                default: false
-            }
-      },
-      data(){
-        return {
-          mapDialogVisible: false
+import LayoutMap from '@/views/map/olMap.vue'
+import { MapManager } from '@/utils/util.map.manage'
+let map;
+let mapManager;
+  export default{
+    name: 'baoZhangMapDialog',
+    components: {
+      LayoutMap,
+    },
+    props:{
+      visible: {
+          type: Boolean,
+          default: false
+      }
+    },
+    data(){
+      return {
+        mapDialogVisible: false
+      }
+    },
+    updated(){
+        debugger;
+        map = this.$refs.olMap.getMap();
+        mapManager = new MapManager(map);
+    },
+    watch:{
+      mapDialogVisible:function(val){
+        if(val){
+          this.init();
+        }
+        else{
+          this.$emit('update:visible', false);
         }
       },
-      watch:{
-        mapDialogVisible:function(val){
-          if(val){
-            this.init();
-          }
-          else{
-            this.$emit('update:visible', false);
-          }
-        },
-        visible:function(val){
-          if(val){
-            this.mapDialogVisible = true;
-          }
-        }
-      },
-      mounted(){},
-      methods:{
-        init(){
-
-        },
-        drawArea(){
-
-        },
-        drawLine(){
-
-        },
-        drawDot(){
-
-        },
-        clearDraw(){
-
-        },
-        saveMap(){
-
-        },
-        resetMap(){
-
-        },
-        handleCancel(){
-          this.mapDialogVisible = false;
+      visible:function(val){
+        if(val){
+          this.mapDialogVisible = true;
         }
       }
-  }
+    },
+    mounted(){
+      this.$nextTick().then(() => {
+        // debugger;
+        // 获取地图
+        // map = this.$refs.olMap.getMap()
+        // mapManager = new MapManager(map)
+        // 将mapManager状态存至vuex
+        // this.setMapManager(mapManager)
+        // map.on('click', this.mapClickHandler)
+      })
+    },
+    methods:{
+      init(){
+
+      },
+      drawDot(){
+        const draw = mapManager.activateDraw('point');
+        console.log(draw);
+        draw.on('drawend', function(e) {
+          console.log(e);
+        })
+      },
+      drawLine(){
+
+      },
+      drawArea(){
+
+      },
+
+      clearDraw(){
+
+      },
+      saveMap(){
+
+      },
+      resetMap(){
+
+      },
+      handleCancel(){
+        this.mapDialogVisible = false;
+      }
+    }
+}
 </script>
 <style lang="scss" scoped>
-    .bao-zhang-map-dialog-wap{
-        .bao-zhang-map-dialog{
-            height: 500px;
-            /deep/.ant-modal{
-                top:0;
-                height:100%;
-            }
-            .yuan_dialog_body{
-                height: 500px;
-                width: 100%;
-                position:relative;
-                .operate-panel{
-                    position: absolute;
-                    top: 20px;
-                    right: 20px;
-                    button{
-                        margin-left: 10px;
-                    }
-                }
-            }
-        }
+.bao-zhang-map-dialog-wap {
+  .bao-zhang-map-dialog {
+    height: 500px;
+    /deep/.ant-modal {
+      top: 0;
+      height: 100%;
     }
+    .yuan_dialog_body {
+      height: 500px;
+      width: 100%;
+      position: relative;
+      .operate-panel {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        button {
+          margin-left: 10px;
+        }
+      }
+    }
+  }
+}
 
-    /*/deep/.ant-modal{*/
-        /*top:0;*/
-        /*height:100%;*/
-    /*}*/
-
+/*/deep/.ant-modal{*/
+/*top:0;*/
+/*height:100%;*/
+/*}*/
 </style>
