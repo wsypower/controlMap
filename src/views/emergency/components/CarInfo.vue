be63ec3893214bb263d895c1246567110a4c18de
nancy12138 <mxj198701@126.com>
2019-09-03 16:46:30 +0800

    周边资源-车辆+GPS详情组件添加

---
+++ src/views/emergency/components/CarInfo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="info">
+        <div><span>车牌：</span><span>{{info.name}}</span></div>
+        <div><span>类型：</span><span>{{info.type}}</span></div>
+        <div><span>GPS：</span><span>{{info.person}}</span></div>
+        <div><span>驾驶员：</span><span>{{info.tel}}</span></div>
+        <div><span>电话：</span><span>{{info.address}}</span></div>
+    </div>
+</template>
+<script type="text/ecmascript-6">
+    export default{
+        name: 'info',
+        props:{
+            info:{
+                type: Object,
+                default(){
+                    return {}
+                }
+            }
+        },
+
+    }
+</script>
+<style lang="scss" scoped>
+    .info{
+        width: 100%;
+        height:100%;
+        padding: 17px;
+        background-color: #ffffff;
+        div{
+            span:first-child{
+                display:inline-block;
+                width: 70px;
+            }
+            span:last-child{
+                display:inline-block;
+                max-width: 200px;
+            }
+        }
+    }
+</style>
