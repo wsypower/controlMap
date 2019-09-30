87740f727c8fa9324ce2254ae77f917da6ad280e
nancy12138 <mxj198701@126.com>
2019-09-30 15:52:14 +0800

    配置文件上传

--- public/config.js
+++ public/config.js
@@ -1,14 +1,14 @@
 const URL_CONFIG = {
-  baseURL: "http://192.168.71.33:8015/api", //测试服务器
+  // baseURL: "http://192.168.71.33:8015/api", //测试服务器
   // baseURL: "http://192.168.71.238:8015/api/", //开发本地后端服务器
   // baseURL: "http://192.168.4.5:81/api/", //正式内网服务器
-  // baseURL: "http://61.153.37.214:81/api/", //正式外网服务器
+  baseURL: "http://61.153.37.214:81/api/", //正式外网服务器
 };
 const GIS_CONFIG={
- // baseURL:"http://61.153.37.212:6350/geoserver/",//正式外网服务器
- baseURL:"http://192.168.1.10:8080/geoserver/",//本地测试服务器
- // featureNS:"http://hescgis",//正式预案区域图层所在工作空间的url
- featureNS:"http://www.haining.com",//测试预案区域图层所在工作空间的url
- // featurePrefix:"hescgis",//正式工作空间名称
- featurePrefix:"haining",//测试工作空间名称
+ baseURL:"http://61.153.37.212:6350/geoserver/",//正式外网服务器
+ // baseURL:"http://192.168.1.10:8080/geoserver/",//本地测试服务器
+ featureNS:"http://hescgis",//正式预案区域图层所在工作空间的url
+ // featureNS:"http://www.haining.com",//测试预案区域图层所在工作空间的url
+ featurePrefix:"hescgis",//正式工作空间名称
+ // featurePrefix:"haining",//测试工作空间名称
 }
