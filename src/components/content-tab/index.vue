0d717b64e7e7a7b36e70cbda8f933ecb12692d7d
nancy12138 <mxj198701@126.com>
2019-10-29 15:00:47 +0800

    修改tab组件，可以配置多个（最多4个），以及是否需要滚动

--- src/components/content-tab/index.vue
+++ src/components/content-tab/index.vue
@@ -1,60 +1,77 @@
 <template>
-    <a-tabs :defaultActiveKey="defaultActiveKey" @change="changeTab" style="height: 100%" class="tab-layer">
-        <a-tab-pane v-for="(item, index) in tabData" :tab="item.name" :key="index">
-            <!--统计分析页面需要全局滚动条，其余页面只是页面中的一部分可能需要滚动显示，故只给统计页面增加全局滚动-->
-            <cg-container scroll v-if="item.name==='统计分析'">
-                <component :is="item.component"></component>
-            </cg-container>
-            <component v-else :is="item.component"></component>
-        </a-tab-pane>
-    </a-tabs>
+  <a-tabs :defaultActiveKey="defaultActiveKey" @change="changeTab" style="height: 100%" class="tab-layer">
+    <a-tab-pane v-for="(item, index) in tabData" :tab="item.name" :key="index">
+      <!--统计分析页面需要全局滚动条，其余页面只是页面中的一部分可能需要滚动显示，故只给统计页面增加全局滚动-->
+      <cg-container scroll v-if="item.hasScroll">
+        <component :is="item.component"></component>
+      </cg-container>
+      <component v-else :is="item.component"></component>
+    </a-tab-pane>
+  </a-tabs>
 </template>
 <script type="text/ecmascript-6">
-    export default {
-        name: 'contentTabs',
-        props:{
-            tabData:{
-                type: Array,
-                default(){
-                    return []
-                }
-            }
-        },
-        data(){
-            return {
-                defaultActiveKey: 0
-            }
-        },
-        methods: {
-            changeTab(val){
-                console.log('changeTab',val);
+export default {
+    name: 'contentTabs',
+    props:{
+        tabData:{
+            type: Array,
+            default(){
+                return []
             }
         }
+    },
+    data(){
+        return {
+            defaultActiveKey: 0
+        }
+    },
+  // computed:{
+  //     nums: function(){
+  //       return this.tabData.length;
+  //     }
+  // },
+    methods: {
+        changeTab(val){
+            console.log('changeTab',val);
+        }
     }
+}
 </script>
 <style lang="scss" scoped>
-.tab-layer{
-    height: 100%;
-    /deep/.ant-tabs-bar{
-        background-color: #f5f5f5;
-        margin-bottom: 3px;
-        padding: 0px 20px;
-        .ant-tabs-nav{
-            width: 100%;
-            .ant-tabs-tab{
-                width: 50%;
-                text-align: center;
-                margin: 0px;
-                font-size: 18px;
-            }
-        }
+@mixin n-items($n) {
+  &:first-of-type:nth-last-of-type(#{$n}),
+  &:first-of-type:nth-last-of-type(#{$n}) ~ & {
+    width: calc(100% / #{$n});
+  }
+}
+.tab-layer {
+  height: 100%;
+  /deep/.ant-tabs-bar {
+    background-color: #f5f5f5;
+    margin-bottom: 3px;
+    padding: 0px 20px;
+    .ant-tabs-nav {
+      width: 100%;
+      .ant-tabs-tab {
+        //width: 50%;
+        text-align: center;
+        margin: 0px;
+        font-size: 18px;
+      }
+      .ant-tabs-tab {
+        @include n-items(1);
+        @include n-items(2);
+        @include n-items(3);
+        @include n-items(4);
+      }
     }
-    /deep/.ant-tabs-content{
-        height: calc(100% - 50px);
-        .ant-tabs-tabpane{
-            position: relative;
-            height: 100%;
-        }
+  }
+  /deep/.ant-tabs-content {
+    height: calc(100% - 50px);
+    .ant-tabs-tabpane {
+      position: relative;
+      height: 100%;
     }
+  }
 }
 </style>
