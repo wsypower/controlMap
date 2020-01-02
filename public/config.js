2e30fae1f1d7c158311caa5df1f95f2578ca7725
nancy12138 <mxj198701@126.com>
2020-01-02 17:24:11 +0800

    URL配置文件修改

--- public/config.js
+++ public/config.js
@@ -4,13 +4,13 @@ const URL_CONFIG = {
   // baseURL: "http://192.168.4.5:81/api/", //正式内网服务器
   // baseURL: "http://61.153.37.214:81/api/", //正式外网服务器
 
-  baseURL2: "http://192.168.71.33:301/api", //测试服务器
+  // baseURL2: "http://192.168.71.33:301/api", //测试服务器
   // baseURL2: "http://192.168.71.238:8015/api/", //开发本地后端服务器
   // baseURL2: "http://192.168.4.5:81/api/", //正式内网服务器
   // baseURL2: "http://61.153.37.214:81/api/", //正式外网服务器
 
   fileURL: "http://192.168.71.33:50000/file/file/uploadFileWeb", //测试文件服务器
-  //fileURL: "http://61.153.37.213:8087/file/file/uploadFileWeb" //正式文件服务器
+  // fileURL: "http://61.153.37.213:8087/file/file/uploadFileWeb" //正式文件服务器
 };
 const GIS_CONFIG={
  // baseURL:"http://61.153.37.212:6350/geoserver",//正式外网服务器
