27c671b4dfb7a0fc6a4d17bc4f718b38838b7d2b
nancy12138 <mxj198701@126.com>
2020-08-29 12:43:42 +0800

    配置文件修改

--- public/js/config.js
+++ public/js/config.js
@@ -4,5 +4,9 @@ const URL_CONFIG = {
   videoURL: "http://202.96.125.73:8692/geapi/", //视频服务器
   // baseURL: "http://60.191.224.129:83/api/", //正式外网服务器
   // baseURL: "http://zhcg.jhk.gov.cn:83/api/", //正式外网域名服务器
-  eventInfoURL: "http://192.168.71.33:81/eventInfo/#/" //事件详情链接
+  eventInfoURL: "http://192.168.71.33:81/eventInfo/#/", //事件详情链接
+  gisApi:'http://192.168.1.10:8080/geoserver/',// 测试gis服务器
+  featurePre:'jinkaiqu',// 测试gis服务器
+  // gisApi:'http://zhcg.jhk.gov.cn:84/geoserver/',// 正式gis服务器
+  // featurePre:'hescgis',// 正式gis服务器
 };
