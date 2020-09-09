51f86ce3222e427a158ec085208c05ba18e48f43
nancy12138 <mxj198701@126.com>
2020-09-09 09:00:47 +0800

    自测问题修复

--- public/config.js
+++ public/config.js
@@ -1,15 +1,15 @@
 const URL_CONFIG = {
   // baseURL: "http://192.168.71.33:8015/api", //测试服务器
-  // baseURL: "http://192.168.71.220:8281/api/", //开发本地后端服务器
+  baseURL: "http://192.168.71.220:8281/api/", //开发本地后端服务器
   // baseURL: "http://192.168.4.5:81/api/", //正式内网服务器
-  baseURL: "http://zhcg.xzzfj.jinhua.gov.cn/api/", //正式外网服务器
+  // baseURL: "http://zhcg.xzzfj.jinhua.gov.cn/api/", //正式外网服务器
   // baseURL: "http://61.153.37.214:81/api/", //正式外网服务器
 
   // baseURL2: "http://192.168.71.33:301/api", //测试服务器
-  // baseURL2: "http://192.168.71.220:8281/api/", //开发本地后端服务器
+  baseURL2: "http://192.168.71.220:8281/api/", //开发本地后端服务器
   // baseURL2: "http://192.168.4.5:81/api/", //正式内网服务器
   // baseURL2: "http://61.153.37.214:81/api/", //正式外网服务器
-  baseURL2: "http://zhcg.xzzfj.jinhua.gov.cn/api/", //正式外网服务器
+  // baseURL2: "http://zhcg.xzzfj.jinhua.gov.cn/api/", //正式外网服务器
 };
 const GIS_CONFIG={
   baseURL:"http://zhcg.xzzfj.jinhua.gov.cn/geoserver/",//正式外网服务器
