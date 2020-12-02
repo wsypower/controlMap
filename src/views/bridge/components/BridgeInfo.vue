6652469f087d1f6839b3d771149f2d736a6db3dd
nancy12138 <mxj198701@126.com>
2020-12-02 16:18:21 +0800

    桥梁机构改为结构类型

--- src/views/bridge/components/BridgeInfo.vue
+++ src/views/bridge/components/BridgeInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="info">
     <div flex><span>桥梁名称：</span><span class="">{{ info.bridgeName }}</span></div>
-    <div flex><span>桥梁机构：</span><span class="">{{ info.bridgeStructure }}</span></div>
+    <div flex><span>结构类型：</span><span class="">{{ info.bridgeStructure }}</span></div>
     <div flex><span>详细地址：</span><span class="value">{{ info.bridgeAddr }}</span></div>
     <div flex><span>完成日期：</span><span class="">{{ info.completeTime }}</span></div>
   </div>
